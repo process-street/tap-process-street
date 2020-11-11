43480caf71cf1133a2cc163c5a5f41c324faa9bd
HerbiePorter <herbieporter@gmail.com>
2020-11-11 12:37:50 +0000

    Generated example tap from singer-tap-template

---
+++ README.md
@@ -0,0 +1,17 @@
+# tap-process-street
+
+This is a [Singer](https://singer.io) tap that produces JSON-formatted data
+following the [Singer
+spec](https://github.com/singer-io/getting-started/blob/master/SPEC.md).
+
+This tap:
+
+- Pulls raw data from (http://process.st)
+- Extracts the following resources:
+  - [FIXME](http://example.com)
+- Outputs the schema for each resource
+- Incrementally pulls data based on the input state
+
+---
+
+Copyright &copy; 2018 Stitch
---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="tap-process-street",
+    version="0.1.0",
+    description="Singer.io tap for extracting data",
+    author="Stitch",
+    url="http://singer.io",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap_process_street"],
+    install_requires=[
+        # NB: Pin these to a more specific version for tap reliability
+        "singer-python",
+        "requests",
+    ],
+    entry_points="""
+    [console_scripts]
+    tap-process-street=tap_process_street:main
+    """,
+    packages=["tap_process_street"],
+    package_data = {
+        "schemas": ["tap_process_street/schemas/*.json"]
+    },
+    include_package_data=True,
+)
