a83e65f370f178574882b4aaa9faca1c456eab26
HerbiePorter <herbieporter@gmail.com>
2020-11-26 09:56:14 +0000

    PS-8913: Add users and simplify catalog generation

--- tap_process_street/schema.py
+++ tap_process_street/schema.py
@@ -1,59 +1,29 @@
-import os
 import json
-from singer import metadata
+import os
 
-from tap_process_street.constant import ID_KEY, UPDATED_DATE_KEY
+STREAMS = [
+    'checklists',
+    'form-field-values',
+    'form-field-widgets',
+    'tasks',
+    'templates',
+    'users'
+]
 
-STREAMS = {
-    'checklists': {
-        'key_properties': [ID_KEY],
-        'replication_method': 'INCREMENTAL',
-        'replication_keys': [UPDATED_DATE_KEY, ID_KEY]
-    },
-    'form-field-values': {
-        'key_properties': [ID_KEY],
-        'replication_method': 'INCREMENTAL',
-        'replication_keys': [UPDATED_DATE_KEY, ID_KEY]
-    },
-    'form-field-widgets': {
-        'key_properties': [ID_KEY],
-        'replication_method': 'INCREMENTAL',
-        'replication_keys': [UPDATED_DATE_KEY, ID_KEY]
-    },
-    'tasks': {
-        'key_properties': [ID_KEY],
-        'replication_method': 'INCREMENTAL',
-        'replication_keys': [UPDATED_DATE_KEY, ID_KEY]
-    },
-    'templates': {
-        'key_properties': [ID_KEY],
-        'replication_method': 'INCREMENTAL',
-        'replication_keys': [UPDATED_DATE_KEY, ID_KEY]
-    }
-}
 
 def get_abs_path(path):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
 
+
 def get_schemas():
     schemas = {}
-    schemas_metadata = {}
 
-    for stream_name, stream_metadata in STREAMS.items():
+    for stream_name in STREAMS:
         schema_path = get_abs_path('schemas/{}.json'.format(stream_name))
 
         with open(schema_path) as file:
             schema = json.load(file)
 
-        meta = metadata.get_standard_metadata(
-            schema=schema,
-            schema_name=stream_name,
-            key_properties=stream_metadata.get('key_properties', None),
-            valid_replication_keys=stream_metadata.get('replication_keys', None),
-            replication_method=stream_metadata.get('replication_method', None)
-        )
-
         schemas[stream_name] = schema
-        schemas_metadata[stream_name] = meta
 
-    return schemas, schemas_metadata
+    return schemas
